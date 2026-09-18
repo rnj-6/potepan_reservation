9eee8b3b0a7614ff0f84fee980b0dc05a5c59732
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-18 22:29:47 +0900

    プロフィールの編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,27 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def profile_edit
+    @user = current_user
+  end
+
+  def profile_update
+    @user = current_user
+
+    if @user.update(user_params)
+      redirect_to users_profile_path
+    else
+      render :profile_edit, status: :unprocessable_entity
+    end
+  end
+
   def account
     @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :image, :introduction, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
     sessions: "users/sessions"
   }
   get "users/profile", to: "users#profile"
+  get "users/profile/edit", to: "users#profile_edit"
+  patch "users/profile", to: "users#profile_update"
+
   get "users/account", to: "users#account"
+  get "users/account/edit", to: "users#account_edit"
+  patch "users/account", to: "users#account_update"
 
   resources :users, only:[:show] #アカウント作成後の画面
   resources :rooms, only:[:index] #サインイン後の画面
