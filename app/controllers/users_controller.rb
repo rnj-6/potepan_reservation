02cb393f50e010250000478ff3830743e990bdcb
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-20 16:01:50 +0900

    アカウント情報と編集画面を装飾

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  # プロフィール
   def profile
     @user = current_user
   end
@@ -21,13 +22,36 @@ class UsersController < ApplicationController
     end
   end
 
+  # アカウント
   def account
     @user = current_user
+  end
+
+  def account_edit
+    @user = current_user
+  end
+
+  def account_update
+    @user = current_user
+
+    if @user.update_with_password(user_params)
+      redirect_to users_account_path
+    else
+      render :account_edit, status: :unprocessable_entity
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :image, :introduction, :email, :password)
+    params.require(:user).permit(
+      :name,
+      :image,
+      :introduction,
+      :email,
+      :password,
+      :current_password,
+      :password_confirmation
+    )
   end
 end
