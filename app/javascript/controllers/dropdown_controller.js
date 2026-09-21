775ac5a8f92e29926a0cb3f5b73f1d1bfdb7ecb3
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-21 14:16:40 +0900

    ドロップダウンリスト表示後他のところをクリックすると閉じる機能を実装

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
  static targets = ["menu"]
+
   toggle() {
     this.menuTarget.classList.toggle("is-open")
+  }
+
+  close(event) {
+    if (!this.element.contains(event.target)) {
+      this.menuTarget.classList.remove("is-open")
+    }
   }
 }
