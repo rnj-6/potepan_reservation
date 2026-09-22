95b337bcfd0c8b9a6dc0095e6d4f27ac630f58b9
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-23 02:55:35 +0900

    予約確認画面を実装

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :user  # 予約した人
   belongs_to :room  # 予約された施設
+
+  def stay_nights
+    (checkout_at - checkin_at).to_i
+  end
+
+  def total_price
+    room.price * stay_nights * guest_count
+  end
 end
