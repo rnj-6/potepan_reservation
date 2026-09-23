49eecc4313a355ab85c339622e02b9fdc7837fab
河内妃奈子 <hina.k0310q@gmail.com>
2026-09-24 00:42:21 +0900

    ...

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user          # 登録者
-  has_many :reservations    # 施設1つあたりの予約
+  has_many :reservations , dependent: :destroy   # 施設1つあたりの予約
   has_one_attached :image
 
   validates :name, presence: true
